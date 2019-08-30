1d92528955042df4fabee36c58fd0f3cde9fb7bf
nguyenvanhoang7398 <nguyenvanhoang7398@gmail.com>
2019-08-30 19:03:37 +0800

    Fix sine generation

--- misc/mock_generator.py
+++ misc/mock_generator.py
@@ -6,21 +6,21 @@ import torch
 def generate_sine_wave(output_path, size):
 	np.random.seed(2)
 
-	label_array = [1, 2, 3, 4]
-
+	label_array = [0, 1, 2, 3]
+	N = 20
+	
 	dataset = []
 	for i in range(size):
 		label = random.choice(label_array)
-		sine_wave = create_sine_wave(size, label)
-		features = " ".join([str(x) for x in list(sine_wave)])
+		sine_wave = create_sine_wave(N, label)
+		features = " ".join([str(sine_wave) for x in list(sine_wave)])
 		dataset.append("\t".join([features, str(label)]))
 	with open(output_path, "w") as f:
 		for row in dataset:
 			f.writelines(row + "\n")
 
-
-def create_sine_wave(size, label):
-	param = label
-	x = np.linspace(-np.pi * np.random.rand(), np.pi * np.random.rand(), size)
-	data = x / param
+def create_sine_wave(N, label):
+	T = label
+	x = np.linspace(-np.pi * np.random.rand(), np.pi * np.random.rand(), N)
+	data = x / (T + 1)
 	return data
